6402f70bca34185fd037763d38864e3c387cbf48
Lukas Gosch <gosch.lukas@gmail.com>
2019-01-21 12:03:53 +0100

    Added b&b api and implementation for the 0/1 knapsack problem.

---
+++ bb.py
@@ -0,0 +1,254 @@
+#######################################################################
+# Author: Lukas Gosch
+# Date: 21.1.2019
+# Description:
+# 	This is a general purpose branch and bound API which supports
+#	three search strategies: best-first, depth-first, breath-first.
+#
+#	To use this API to solve a certain problem using B&B, a class
+#	representing both the problem to solve as well as an instance of 
+#	this problem has to be created. This class must inherit from the 
+#	Instance class and overwrite it's "abstract" functions such as  
+#	lower and upper bound calculation and branching. Given a user has 
+#	implemented a class Problem inheriting from Instance and has 
+#	initialized an object obj of type Problem. She can then drive
+#	the optimization through calling obj.solve().
+#
+#	The API can be used to solve min- and maximization problems. If
+#	a certain problem instance is a minimization or maximization 
+#	problem has to be told the API through overwriting the function 
+#	isMax().
+#
+#	An example implementation for the 0/1 Knapsack problem is provided 
+#	through a class Knapsack inheriting from Instance, see Knapsack.py 
+#	
+#	A custom search strategy can be added by modifying the methods 
+#	in the InstanceSet class.
+#######################################################################
+
+import sys
+import importlib
+import argparse
+import queue
+import copy
+import numpy as np
+
+class Instance:
+	""" Abstract Problem Instance Class, if one wants to add a Problem
+		Class to solve using B&B, it must inherit from Instance.
+	"""
+
+	def isMax(self):
+		""" Return True if optimization problem is a maximization problem. """
+		raise NotImplementedError("isMax implementation missing.")
+
+	def calcUpperBound(self):
+		""" Calculate upper bound value. 
+			
+			If the problem is a minimization problem, set valid heuristic
+			solution.
+		"""
+		raise NotImplementedError("calcUpperBound implementation missing.")
+
+	def calcLowerBound(self):
+		""" Calculate lower bound value. 
+			
+			If the problem is a maximiztion problem, set valid heuristic
+			solution.
+		"""
+		raise NotImplementedError("calcLowerBound implementation missing.")
+
+	def getUpperBound(self):
+		""" Return upper bound value. """
+		raise NotImplementedError("getUpperBound implementation missing.")
+
+	def getLowerBound(self):
+		""" Return lower bound value. """
+		raise NotImplementedError("getLowerBound implementation missing.")
+
+	def isUpperBoundSet(self):
+		""" Return true if an up-to-date upper bound has been calculated. """
+		raise NotImplementedError("isUpperBoundSet implementation missing.")
+
+	def isLowerBoundSet(self):
+		""" Return true if an up-to-date lower bound has been calculated. """
+		raise NotImplementedError("isLowerBoundSet implementation missing.")
+
+	def genInitialSolution(self):
+		""" Generate an initial heuristic solution. """
+		raise NotImplementedError("genInitialSolution implementation missing.")
+
+	def getHeuristicSolution(self):
+		""" Return a valid heuristic solution. 
+
+			The returned solution should correspond to the lower bound
+			(upper bound) value in the maximization (minimization) problem.
+			The solve() methods ensures that this function is only called
+			if isLowerBoundSet() (isUpperBoundSet()) returns true.
+		"""
+		raise NotImplementedError("getHeuristicSolution implementation missing.")
+
+	def getHeuristicSolutionValue(self):
+		""" Return value of the heuristic solution. """
+		if self.isMax():
+			return self.getLowerBound()
+		return self.getUpperBound()
+
+	def branch(self):
+		""" Generate and return iterator over subinstances. """
+		raise NotImplementedError("branch implementation missing.")
+
+	def solve(self, search_strategy = 'best_first', max_branches = -1):
+		""" Solve problem using branch & bound.
+
+			search_strategy ... specify a ceratin search strategy for
+								the branch & bound
+			max_branches	...	specify a maximum number of branches
+								generated before the algorithm should
+								stop 
+			
+			Returns the found solution, its value, the number of branches
+			generated and the number of investigated instances.
+		"""
+		# Count number of branches and iterations
+		branch_c = 0
+		iter_c = 0
+
+		# Initialize best solution as initial solution
+		self.genInitialSolution()
+		best_I = self
+
+		I_set = InstanceSet(self, search_strategy)
+		# Evaluate promising instances until no promising instance left
+		# or (if set) maximum number of branch-processes reached.
+		while (not I_set.isEmpty()
+			   and (branch_c < max_branches or max_branches == -1)):
+			# Remove next instance from instance set
+			I = I_set.getNextInstance()
+			# Look at instance if it could contain a better global solution
+			if I.isPromising(best_I):
+				if I.isBetterThen(best_I):
+					best_I = I
+				# Branch current instance if still promising
+				if I.isPromising(best_I):
+					for sub_I in I.branch():
+						# Add subinstance to instance set if promising
+						if sub_I.isPromising(best_I):
+							I_set.addInstance(sub_I)
+					branch_c += 1
+			iter_c += 1
+
+		opt_solution = best_I.getHeuristicSolution()
+		opt_value = best_I.getHeuristicSolutionValue()
+		return opt_solution, opt_value, branch_c, iter_c
+
+	def isPromising(self, current):
+		""" Return True if instance self could contain a better
+			solution then the heuristic solution set in the 
+			current instance.
+		"""
+		if self.isMax():
+			if not self.isUpperBoundSet():
+				self.calcUpperBound()
+			if self.getUpperBound() > current.getLowerBound():
+				return True
+		else:
+			if not self.isLowerBoundSet():
+				self.calcLowerBound()
+			if self.getLowerBound() < current.getUpperBound():
+				return True
+		return False
+
+	def isBetterThen(self, current):
+		""" Return True if self contains a better heuristic solution
+			then current.
+		"""
+		if self.isMax():
+			if not self.isLowerBoundSet():
+				self.calcLowerBound()
+			return self.getLowerBound() > current.getLowerBound()
+		else:
+			if not self.isUpperBoundSet():
+				self.calcUpperBound()
+			return self.getUpperBound() < current.getUpperBound()
+
+	def __lt__(self, other):
+		""" Return True if instance self holds potentially
+			better solutions then instance other. 
+		"""
+		if self.isMax():
+			return self.getUpperBound() > other.getUpperBound()
+		else:
+			return self.getLowerBound() < other.getLowerBound()
+
+class InstanceSet:
+	""" Holds a set of Instances of a certain problem. """
+
+	def __init__(self, instance, search_strategy):
+		""" Initializes a set of instances for a certain optimization problem.
+			
+			The data structure used to store subinstances of a problem depends
+			on the chosen search_strategy.
+
+			search_strategies: 
+			-'best_first': priority queue is used, provides fast adding
+						   and removing of instances from the instance
+						   set (O(log n)). Definition of "best" depends
+						   on maximization or minimization problem.
+			-'depth_first': O(1) adding and removing
+			-'breath_first': O(1) adding and removing
+		"""
+		self._sstrategy = search_strategy
+		if self._sstrategy == 'best_first':
+			self._iset = queue.PriorityQueue()
+			# Check if bound for sorting is set
+			if instance.isMax():
+				if not instance.isUpperBoundSet():
+					instance.calcUpperBound()
+			else:
+				if not instance.isLowerBoundSet():
+					instance.calcLowerBound()
+			self.addInstance(instance)
+		elif self._sstrategy == 'depth_first':
+			self._iset = queue.LifoQueue()
+			self.addInstance(instance)
+		elif self._sstrategy == 'breath_first':
+			self._iset = queue.Queue()
+			self.addInstance(instance)
+		else:
+			sys.exit("Search strategy not supported by InstanceSet.")
+
+	def isEmpty(self):
+		""" Return true if no problem instances are in the set. """
+		if self._sstrategy == 'best_first':
+			return self._iset.empty()
+		elif self._sstrategy == 'depth_first':
+			return self._iset.empty()
+		elif self._sstrategy == 'breath_first':
+			return self._iset.empty()
+
+	def getNextInstance(self):
+		""" Remove and return the next instance in the instance set. 
+			Time complexity: - 'best_first': O(log n)
+							 - 'depth_first': O(1)
+							 - 'breath_first': O(1)
+		"""
+		if self._sstrategy == 'best_first':
+			return self._iset.get()
+		elif self._sstrategy == 'depth_first':
+			return self._iset.get()
+		elif self._sstrategy == 'breath_first':
+			return self._iset.get()
+
+	def addInstance(self, instance):
+		""" Add instance to the instance set. 
+			Time complexity: - 'best_first': O(log n)
+							 - 'depth_first': O(1)
+							 - 'breath_first': O(1)
+		"""
+		if self._sstrategy == 'best_first':
+			self._iset.put(instance)
+		elif self._sstrategy == 'depth_first':
+			self._iset.put(instance)
+		elif self._sstrategy == 'breath_first':
+			self._iset.put(instance)
---
+++ knapsack.py
@@ -0,0 +1,173 @@
+#######################################################################
+# Author: Lukas Gosch
+# Date: 21.1.2019
+# Description:
+# 	Implementation of an Instance class in the b&b api for the 0/1 
+#	Knapsack problem.
+#	
+#	Usage: 1) Initialize a Knapsack object with a certain capacity,
+#		   list of item values and weights (see constructor)
+#		   2) Call its solve() method
+#######################################################################
+
+import numpy as np
+from bb import Instance
+
+class Knapsack(Instance):
+
+	def __init__(self, capacity, values, weights, int_problem = False):
+		""" Initialize a Knapsack problem and the initial solution. 
+			
+			capacity ... maximal capacity of the rucksack
+			values ... array-like object storing the values of each item
+			weights ... array-like object storing the weights of each item
+			int_problem ... True if all values are integer numbers.
+		"""
+		# Problem specific data attributes
+		self.max_capacity = np.array(capacity)
+		self.values = np.array(values)
+		self.weights = np.array(weights)
+		self.vw_frac = self.values / self.weights
+		# Get indices that would sort the value-weight fraction array (introsort)
+		self.vw_frac_sortI_ascending = np.argsort(self.vw_frac, axis=-1, 
+												  kind='quicksort')
+		self.vw_frac_sortI = np.flip(self.vw_frac_sortI_ascending)
+		self.n_items = self.values.shape[0]
+		self.int_problem = int_problem
+		
+		# Instance specific data attributes
+		self.solution = np.zeros((self.n_items,))
+		self.index = 0
+		self.curr_weight = 0
+		self.curr_value = 0
+		self.heur_solution = None
+		self.heur_weight = None
+		self.lower_bound = None
+		self.upper_bound = None
+
+	@classmethod
+	def copy(cls, origin):
+		""" Generate a new Knapsack instance based on a given instance.
+			
+			Shallow copy problem specific data attributes, deeply
+			copy instance specific data attributes. 
+
+			Time complexity: O(n)
+		"""
+		new_instance = copy.copy(origin)
+		new_instance.solution = np.copy(origin.solution)
+		if new_instance.isLowerBoundSet():
+			new_instance.heur_solution = np.copy(origin.heur_solution)
+		return new_instance
+
+	def includeItem(self, item, index):
+		""" Add item to the solution. 
+
+			item ... index of the item w.r.t. the values and weights array
+			index ... index of the item in the sorted vw_frac array
+			
+			Return true if item was sucessfully added (i.e. knapsack
+			had enough capacity left). Update instance specific
+			data attributes. 
+		"""
+		if self.curr_weight + self.weights[item] > self.max_capacity:
+			return False
+		self.solution[item] = 1
+		self.index = index + 1
+		self.curr_value += self.values[item]
+		self.curr_weight += self.weights[item]
+		self.heur_solution = None
+		self.heur_weight = None
+		self.lower_bound = None
+		self.upper_bound = None
+		return True
+
+	def isMax(self):
+		""" Return True if optimization problem is a maximization problem. """
+		return True
+
+	def calcUpperBound(self):
+		""" Calculate upper bound value.
+			
+			Greedily add items into knapsack based on best value/weight 
+			fractions of the items. First item which cannot be added to
+			the knapsack is fractionally added to it based on the rest
+			capacity of the knapsack. Upper bound rounded down if 
+			specific Knapsack problem is set to be an integer problem.
+
+			Time Complexity: O(n)
+		"""
+		self.upper_bound = self.curr_value
+		weight = self.curr_weight
+		for item in self.vw_frac_sortI[self.index:]:
+			p_weight = weight + self.weights[item]
+			if p_weight <= self.max_capacity:
+				weight = p_weight
+				self.upper_bound += self.values[item]
+			else:
+				self.upper_bound += (self.max_capacity - weight) \
+									* self.vw_frac[item]
+				break
+		if self.int_problem:
+			self.upper_bound = int(self.upper_bound)
+		
+	def calcLowerBound(self):
+		""" Calculate lower bound value and set heuristic solution.
+			
+			Greedily add items into knapsack based on best value/weight 
+			fractions of the items, traverses all possible items.
+
+			Time Complexity: O(n) 
+		"""
+		self.lower_bound = self.curr_value
+		self.heur_solution = np.copy(self.solution)
+		self.heur_weight = self.curr_weight
+		for item in self.vw_frac_sortI[self.index:]:
+			p_weight = self.heur_weight + self.weights[item]
+			if p_weight <= self.max_capacity:
+				self.heur_weight = p_weight
+				self.heur_solution[item] = 1
+				self.lower_bound += self.values[item]
+
+	def getUpperBound(self):
+		""" Return upper bound value. """
+		return self.upper_bound
+
+	def getLowerBound(self):
+		""" Return lower bound value. """
+		return self.lower_bound
+
+	def isUpperBoundSet(self):
+		if self.upper_bound is None:
+			return False
+		return True
+
+	def isLowerBoundSet(self):
+		if self.lower_bound is None:
+			return False
+		return True
+
+	def genInitialSolution(self):
+		self.calcLowerBound()
+
+	def getHeuristicSolution(self):
+		""" Return greedy solution corresbonding to the lower bound. """
+		return self.heur_solution
+
+	def branch(self):
+		""" Branch knapsack instance into subinstances. 
+			
+			Returns list of subinstances where in each case one distinct
+			item was added to the solution. Return in reverse order
+			to allow for left depth first instead of right depth first
+			exploration if search strategy is set to depth first.
+
+			Time Complexity: O(n^2) (dominant)
+		"""
+		subinstance_l = []
+		for index in range(self.n_items-1, self.index-1, -1):
+			item = self.vw_frac_sortI[index]
+			subinstance = Knapsack.copy(self)
+			if(subinstance.includeItem(item, index)):
+				subinstance_l.append(subinstance)
+		return subinstance_l
